f9b1afea67b8e2946290cd440aad54bc61f0ced3
datuanho2k9 <datuanho2k9@gmail.com>
2021-07-17 20:06:57 +0700

    Fixed timer and some stuff, waiting for rework

--- Synnefo/src/Pages/Open.js
+++ Synnefo/src/Pages/Open.js
@@ -1,65 +1,54 @@
 import firebase from "firebase";
 import React from "react";
 import "./Open.css";
-var timeInseconds;
+var startTime;
+var endTime;
 var url = null;
-function resizeIframe(obj) {
-    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
-}
-
 export default class Open extends React.Component {
     constructor() {
         super()
-        this.state ={
-            code : null,
+        this.state = {
+            code: null,
         }
         this.btnRef = React.createRef();
         this.handleCodeChange = this.handleCodeChange.bind(this)
-        
+
         this.showDoc = this.showDoc.bind(this)
         this.stopViewing = this.stopViewing.bind(this)
     }
-
-    handleCodeChange(event){
+    handleCodeChange(event) {
         this.setState({
-            code : event.target.value
+            code: event.target.value
         })
         this.btnRef.current.style.backgroundColor = "#5865F2"
     }
-    stopViewing(){
+    stopViewing() {
         url = null;
-        seconds = parseInt(document.getElementById("timer").innerText);// here is your seconds
-
-
-        window.location = "/";
-        
-        
-
+        let date = new Date();
+        endTime = date.getSeconds();
+        let time = endTime - startTime;
+        //   time is the var u need;
+        window.location = "/"
     }
-    showDoc(){
+    showDoc() {
         let code = this.state.code;
         let docContentId = document.getElementById("docContent")
-        
-         //change url variable, do some stuff and insert url here
-        url;
+        //change url variable, do some stuff and insert url here
         //....
-
-        if(url !== null){
+       
+        
+        if (url !== null) {
             docContentId.style.display = "block"
-            document.getElementById("doc").src = url;
-            resizeIframe(document.getElementById("doc"))
-
-            for(let i = 0; url !==null; i++){
-                    setTimeout(function(){
-                        document.getElementById("timer") = i
-                    }, 1000)
-
-                    
-                    
-            }
-
-
-        }else{
+            document.getElementById("doc").src = url
+            let date = new Date();
+            startTime = date.getSeconds()
+            //for looping
+            let second = 0;
+            setInterval(() => {
+                document.getElementById("timer").innerText = second.toString();
+                second++
+            }, 1000);
+        } else {
             document.getElementById("error").style.display = "block"
 
         }
@@ -76,14 +65,14 @@ export default class Open extends React.Component {
                         <div className="codeInput">
                             <p>Input the code to open the document</p>
                             <input type="text" onChange={this.handleCodeChange}>
-                            
+
                             </input><br />
-                            <p style={{color: "red", display : "none"}} id="error">Your code is invalid</p>
+                            <p style={{ color: "red", display: "none" }} id="error">Your code is invalid</p>
                             <button ref={this.btnRef} onClick={this.showDoc}>View</button>
 
                         </div>
-                        <div id="docContent" style={{display : "none"}}>
-                            <p style={{textAlign : "left"}}>You have been viewing this document for: <p id="timer"></p></p>
+                        <div id="docContent" style={{ display: "none" }}>
+                            <p >You have been viewing this document for: <span id="timer"> 0 </span> seconds</p>
                             <iframe id="doc"></iframe>
                             <button onClick={this.stopViewing}>Stop viewing</button>
 
