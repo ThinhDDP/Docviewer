731b97fab616e05910f4958b18f67e5fc2788d49
datuanho2k9 <datuanho2k9@gmail.com>
2021-07-25 11:03:40 +0700

    Make template for Recent.js

---
+++ Synnefo/src/Pages/Recent.js
@@ -0,0 +1,15 @@
+import React from "react";
+export default class Recent extends React.Component {
+    constructor(){
+        super()
+        console.log("This is a useless-constructor")
+    }
+    render(){
+        return(
+        <div>
+            <p style={{textAlign: "center"}}>Hi</p>
+
+        </div>
+        )
+    }
+}
