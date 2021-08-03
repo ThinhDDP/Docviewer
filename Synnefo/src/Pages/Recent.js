aa7fe1d5b9b27a1cbe629dbb7f5f31e2b4b0a92b
datuanho2k9 <datuanho2k9@gmail.com>
2021-08-03 22:02:33 +0700

    Fix some css, done Recent.js cards

--- Synnefo/src/Pages/Recent.js
+++ Synnefo/src/Pages/Recent.js
@@ -5,102 +5,106 @@ import 'react-tabs/style/react-tabs.css';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 
+
 export default class Recent extends React.Component {
     constructor() {
         super()
         this.state = {
             code: 9021,
-            viewedData: [],
-            completedData: [],
+            viewedDocs: {},
+            completedDocs: {},
 
         }
         this.handleViewDocClick = this.handleViewDocClick.bind(this)
-        this.renderCards = this.renderCards.bind(this)
+        // this.renderCards = this.renderCards.bind(this)
 
 
 
     }
     componentDidMount() {
-        let data = {
-            "viewed": ["abcdefg", "abcdefg", "abcdefg", "abcdefg", "abcdefg", "abcdefg", "abcdefg",],
-            "completed": ["dewdew", "dwedwdwd", "dwdew"]
-        }
 
+        let completedDocs = {
+            "abcdefg": "dwdewdw",
+            "abcdefg1": "whatareyouupto",
+            "abcdefg2": "welcomeToMyChannel",
+            "abcdef3g": "Never gonna give up you up",
+            "abcdefg3": "hello friends",
+            "abcdefg4": "what the heck",
+            "abcdef23g": "ching cheng hanj",
+        }
+        let viewedDocs = {
+            "viewedviewedveiwed12": "viewedDOcumentConfirm",
+            "wdwhat": "test1",
+            "test2": "test3",
+            "test4": "tes5",
+            "test6": "test7",
+            "test8": "test9"
+        }
         this.setState({
-            viewedData: data["viewed"],
-            completedData: data["completed"],
+            completedDocs: completedDocs,
+            viewedDocs: viewedDocs
         });
-        console.log(data)
-        this.renderCards(data["completed"], data["viewed"]);
-    }
-
-    renderCards(completed, viewed) {
-        console.log(viewed.length)
-        let viewed_length = viewed.length
-        if (document.getElementById("viewedSection") == null) {
-            document.getElementById("completedSection").innerHTML = ""
-            for (let i = 0; i < completed.length; i++) {
-                document.getElementById("completedSection").innerHTML +=
-                `<div className ="cardDoc">
-                    <p>${completed[i]}</p>
-                    <button onClick=${this.handleViewDocClick} id=${completed[i]}>View document</button>    
-                </div>`
-            }
-        } else {
-            document.getElementById("viewedSection").innerHTML = ""
-            for (let i = 0; i < viewed_length; i++) {
-                document.getElementById("viewedSection").innerHTML +=
-                    `
-                <div className="cardDoc">
-                    <p>${viewed[i]}</p>
-                    <button onClick=${this.handleViewDocClick} id=${viewed[i]}>View document</button>
-                </div>
-                `
-
-            }
-        }
-
     }
     handleViewDocClick(event) {
         this.setState({ code: event.target.id })
-        document.getElementById("docLists").style.display = "none"
+        document.getElementById("niceLists").style.display = "none"
         document.getElementById("viewDoc").style.display = "block"
 
-    }
-    handleTabClick(selectedTab, unselectedTab) {
-        this.renderCards(this.state.completedData, this.state.viewedData)
+
     }
     render() {
-        return (
-            <div className="wrapper">
-                <div id="docLists">
+        let completedDocs = this.state.completedDocs;
+        let viewedDocs = this.state.viewedDocs;
 
+        let arrayDocs_viewed = [];
+        let arrayDocs_completed = [];
 
-                    <Tabs onSelect={(firstTab, lastTab) => this.handleTabClick(firstTab, lastTab)}>
-                        <TabList>
-                            <Tab>Viewed Documents</Tab>
-                            <Tab>Completed Documents</Tab>
-                        </TabList>
-                        <TabPanel>
-                            <div id="viewed">
-                                <h3>Viewed Documents</h3>
-                                <div className="sectionRecent" id="viewedSection"></div>
-
-
-
-                            </div>
-                        </TabPanel>
-                        <TabPanel>
-                            <div id="completed">
-                                <h3>Completed Documents</h3>
-                                <div className="sectionRecent" id="completedSection"></div>
-                                </div>
-
-                        </TabPanel>
-                    </Tabs>
-
+        for (const document_id in completedDocs) {
+            arrayDocs_completed.push(
+                <div className="cardDoc">
+                    <p>{completedDocs[document_id]}</p>
+                    <button onClick={this.handleViewDocClick} id={document_id}>View document</button>
                 </div>
+            )
+        }
+        for (const document_id in viewedDocs) {
+            arrayDocs_viewed.push(
+                <div className="cardDoc">
+                    <p>{viewedDocs[document_id]}</p>
+                    <button onClick={this.handleViewDocClick} id={document_id}>View document</button>
+                </div>
+            )
+        }
 
+
+        return (
+            <div>
+                <div className="wrapper" id="niceLists">
+                    <div id="docLists">
+                        <Tabs>
+                            <TabList>
+                                <Tab>Viewed Documents</Tab>
+                                <Tab>Completed Documents</Tab>
+                            </TabList>
+                            <TabPanel>
+                                <div id="viewed">
+                                    <h3>Viewed Documents</h3>
+                                    <div className="sectionRecent" id="viewedSection">
+                                        {arrayDocs_viewed}
+                                    </div>
+                                </div>
+                            </TabPanel>
+                            <TabPanel>
+                                <div id="completed">
+                                    <h3>Completed Documents</h3>
+                                    <div className="sectionRecent" id="completedSection">
+                                        {arrayDocs_completed}
+                                    </div>
+                                </div>
+                            </TabPanel>
+                        </Tabs>
+                    </div>
+                </div>
                 <div id="viewDoc" style={{ display: "none" }}>
                     <Open code={this.state.code} />
                 </div>
