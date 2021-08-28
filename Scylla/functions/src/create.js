0880f2c7718f4e076a78a435411257f1f5a967d6
datuanho2k9 <datuanho2k9@gmail.com>
2021-08-28 19:41:38 +0700

    Added scylla, fixed mail url

---
+++ Scylla/functions/src/create.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const admin = require('firebase-admin')
+
+const auth = require('../middlewares/auth')
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+const createCode = async () => {
+    let code = makeid(4)
+    let docRef = db.collection('Document').doc(code)
+    let returnCode = await db.runTransaction((transaction) => {
+        return transaction.get(docRef).then((docRef) => {
+            if (!docRef.exists){
+                return docRef.id
+            }
+            else {
+                createCode()
+            }
+        })
+    })
+    return returnCode
+}
+
+
+const db = admin.firestore()
+const router = express.Router()
+
+const toUid = async (email) => {
+    console.log(email)
+    let userRecord = await admin.auth().getUserByEmail(email)
+    return userRecord.uid
+}
+
+const savedOwned = async (emails, docRef, title) => {
+    let uid = await toUid(emails[0])
+    console.log(uid)
+    let usrRef = db.collection('Users').doc(uid)
+    let usrCompleted = await (await usrRef.get()).get('owned')
+    usrCompleted[docRef.id] = title
+    console.log(usrCompleted)
+    usrRef.update({owned: usrCompleted})
+    for (let i = 1; i < emails.length; ++i){
+        let uid = await toUid(emails[i])
+        console.log(uid)
+        let usrRef = db.collection('Users').doc(uid)
+        let usrCompleted = await (await usrRef.get()).get('shared')
+        usrCompleted[docRef.id] = title
+        console.log(usrCompleted)
+        usrRef.update({shared: usrCompleted})
+    }
+}
+
+router.post('/create', auth, async (req, res) => {
+        const code =  await createCode()
+        let docRef = db.collection('Document').doc(code)
+        const data = {
+            id: req.body.id,
+            views: 0,
+            completed: {},
+            emails: [''],
+            time: 0,
+            author: req.body.uid,
+            perm: req.body.perm,
+            title: req.body.name,
+            type: req.body.type
+        }
+        const saved = await savedOwned(req.body.uid, docRef, req.body.name)
+        const rest = await docRef.set(data)
+        if (rest){res.send(code)}
+    }
+
+)
+
+
+module.exports = router
---
+++ Scylla/functions/src/mail.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const admin = require('firebase-admin')
+const nodemailer = require("nodemailer");
+
+const db = admin.firestore()
+const router = express.Router()
+
+const sendMail = async (code, emails, author) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'docviewerdemo@gmail.com',
+            pass: 'Testing123!'
+        }
+    })
+    console.log(author)
+    const mailOptions = {
+        from: 'docviewer@gmail.com',
+        to: emails,
+        subject: 'Docviewer invite code',
+        html: `<h3>User ${author} has invite you to view his document</h3>
+        <p>Click on the link below to view the document</p></br>
+                <p><a href="https://docviewerapi.web.app/open?code=${code}">localhost:3000/open?code=${code}</a></p>
+        `
+    }
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error){
+            console.log(error)
+        }
+        else {
+            console.log(`Email sent ${info.response}`)
+        }
+    })
+}
+
+router.post('/mail/:code', async (req, res) => {
+    let emails = req.body.email
+    let code = req.params.code
+    let author = req.body.author
+    sendMail(code, emails, author).then(() => {
+        res.send('Done')
+    }).catch((e) => {
+        console.log(e)
+        res.send("Unknown error has occured")
+    })
+
+})
+
+module.exports = router
